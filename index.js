3f5f4c2159899e2bcf78571fa085d85d54fe88a9
Shuyibe48 <gthrough48@gmail.com>
2023-06-06 01:41:30 +0600

    save user info on db

--- index.js
+++ index.js
@@ -2,24 +2,76 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb')
 const port = process.env.PORT || 5000
 
 
 // middleware
 const corsOptions = {
-  origin: '*',
-  credentials: true,
-  optionSuccessStatus: 200,
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
 }
 app.use(cors(corsOptions))
 app.use(express.json())
 
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jukjd3u.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const usersCollection = client.db('aircncDb').collection('users')
+        const roomsCollection = client.db('aircncDb').collection('rooms')
+        const bookingsCollection = client.db('aircncDb').collection('bookings')
+
+
+        // save user email and role in db
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email // i am receiving the user email which is sent form client site by params
+            const user = req.body // from the client site i send a data for user role that is he/she host or normal user by body.
+            const query = { email: email } // query mean filter || i am finding exact user by email. the email came from client site by params. 
+            const options = { upsert: true } // upsert mean: if the user does not exist than the user will save as a new user.
+
+            const updateDoc = {
+                $set: user
+            } // TODO
+
+            const result = await usersCollection.updateOne(query, updateDoc, options)
+
+            console.log(result);
+            res.send(result)
+        })
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
 app.get('/', (req, res) => {
-  res.send('AirCNC Server is running..')
+    res.send('AirCNC Server is running..')
 })
 
 app.listen(port, () => {
-  console.log(`AirCNC is running on port ${port}`)
+    console.log(`AirCNC is running on port ${port}`)
 })
